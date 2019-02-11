842e52b1cf53c46ed14c14b3b8db7a210f070421
joselevelh <43297291+joselevelh@users.noreply.github.com>
2019-02-11 14:56:31 -0500

    Opens nice csv files
    Cannot  transpose csv files if the last rows are uneven lengths.

--- OpenCSV.py
+++ OpenCSV.py
@@ -6,15 +6,27 @@ root = Tk()
 root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("csv files","*.CSV"),("all files","*.*")))
 print (root.filename)
 
-#with open(root.filename, newline='') as csvfile:
-    #LucasCSV = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    #for row in LucasCSV:
-        #print(', '.join(row))
-def getCols(rList,numElements):
-    for rows in rList:
-        for cols in rList[rows]:
-            NewList[cols][rows]=rList[rows][cols]
-    return NewList
-testList=[[1,2,3],[4,5,6],[7,8,9]]
-#getCols(testList,9)
-print(getCols(testList,9))
+csvList=[]
+csvList=list(csv.reader(open(root.filename)))#This puts csv files into a 2d list
+print(csvList)
+
+print("Rows: =",len(csvList),"\ncollumns: ",len(csvList[0]))
+
+def csvTruncate(uFile):
+
+def transpose(OldList): #transpose the 2d list given
+    newlist = []
+    i = 0
+    while i < len(OldList[i]):
+        j = 0
+        vec = []
+        while j < len(OldList):
+            vec.append(OldList[j][i])
+            j = j + 1
+        newlist.append(vec)
+        i = i + 1
+    return newlist
+#print (transpose(csvList))
+#Stuff=[[1,2],[4,5],[7,8]]
+#newStuff=transpose(Stuff)
+#print (newStuff)
