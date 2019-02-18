e3758c1435e3f0ede94e213009b62a8367c4a083
joselevelh <43297291+joselevelh@users.noreply.github.com>
2019-02-18 12:13:08 -0500

    Prints statistics on a text file
    Prints statistics onto Output.txt

--- OpenCSV.py
+++ OpenCSV.py
@@ -28,31 +28,37 @@ def transposeCsv(OldList): #transpose the 2d list given
         i = i + 1
     return newlist
 
-def printData(niceData):    # parse the truncated 2d list for relevant data and print it nicely.
-    numCols=len(niceData)   #Amount of columns in niceData
-    i=1    #We want to skip the first Column because it is the time Column and therefore does not contain integer data.
-    while (i<numCols):   #go through every Column.
-        cTitle=niceData[i][0]   #The first element of each Column is the title of that Column.
-        cData=niceData[i][1:]   #The rest of the elements in each Column are data strings.
-        print ("This is the title: ",cTitle)
-        print (" This is the Data as strings: ",cData)
-        cData=list(map(int,cData))
-        print (" This is the Data as integers: ",cData)
-        #cData=list(map(int,cData[i]))   #Make the list of strings into integers.
-        cMean= mean(cData)
-        cStdev= stdev(cData)
-        cVar= variance(cData)
-        cMin=min(cData)
-        cMax=max(cData)
-        print (" This is the mean: ",cMean)
-        print (" This is the Standard Deviation: ",cStdev)
-        print (" This is the Variance: ",cVar)
-        print (" This is the min: ",cMin)
-        print (" This is the max: ",cMax,"\n")
-        i+=1
-#    with open("Output.txt", "w") as text_file:
-#        print("Column: {}".format(ColumnData), file=text_file)
-#        print("Mean: {}".format(ColumnMean), file=text_file)
+def printData(niceData):    # parse the truncated 2d list for relevant data and print it nicely onto a text file.
+    with open("Output.txt", "w") as text_file:
+        numCols=len(niceData)   #Amount of columns in niceData
+        i=1    #We want to skip the first Column because it is the time Column and therefore does not contain integer data.
+        while (i<numCols):   #go through every Column.
+            cTitle=niceData[i][0]   #The first element of each Column is the title of that Column.
+            cData=niceData[i][1:]   #The rest of the elements in each Column are data strings.
+            #print ("This is the title: ",cTitle)
+            #print (" This is the Data as strings: ",cData)
+            cData=list(map(int,cData))
+        #    print (" This is the Data as integers: ",cData)
+            #cData=list(map(int,cData[i]))   #Make the list of strings into integers.
+            cMean= mean(cData)
+            cStdev= stdev(cData)
+            cVar= variance(cData)
+            cMin=min(cData)
+            cMax=max(cData)
+        #    print (" This is the mean: ",cMean)
+        #    print (" This is the Standard Deviation: ",cStdev)
+        #    print (" This is the Variance: ",cVar)
+        #    print (" This is the min: ",cMin)
+        #    print (" This is the max: ",cMax,"\n")
+            i+=1
+            print("Title: {}".format(cTitle), file=text_file)
+            print("Data: {}".format(cData), file=text_file)
+            print("Mean: {}".format(cMean), file=text_file)
+            print("Standard Deviation: {}".format(cStdev), file=text_file)
+            print("Variance: {}".format(cVar), file=text_file)
+            print("Min: {}".format(cMin), file=text_file)
+            print("Max: {}".format(cMax), file=text_file)
+            print("\n", file=text_file)
 
 def removeBlanks(bList): #bList is a list of lists with elements that may be empty strings, we want to remove these empty strings.
     wBlanks=bList
