75aaafc13f819fa7741c7c4e6950a0a4b4fc8137
joselevelh <43297291+joselevelh@users.noreply.github.com>
2019-02-18 16:37:12 -0500

    NewMean fucntion added

--- OpenCSV.py
+++ OpenCSV.py
@@ -5,6 +5,14 @@ from statistics import mean
 from statistics import stdev
 from statistics import variance
 
+def newMean(list0):   #gets the mean of the list not including zeros.
+    new=[]
+    i=0
+    while i<len(list0):
+        if not list0[i]==0:
+            new.append(list0[i])
+        i+=1
+    return mean(new)
 
 def truncateCsv(uList):   #Truncates the 2d array until all Columns have the same number of Rows
     tempList=uList
@@ -31,37 +39,30 @@ def transposeCsv(OldList): #transpose the 2d list given
 def printData(niceData):    # parse the truncated 2d list for relevant data and print it nicely onto a text file.
     with open("Output.txt", "w") as text_file:
         print("Hey ${PERSON_NAME}, \n This email should contain the data you are looking for -Jose Level \n ", file=text_file)
+        print("File Opened: {}\n".format(root.filename), file=text_file)
         numCols=len(niceData)   #Amount of columns in niceData
         i=1    #We want to skip the first Column because it is the time Column and therefore does not contain integer data.
         while (i<numCols):   #go through every Column.
             cTitle=niceData[i][0]   #The first element of each Column is the title of that Column.
-            cData=niceData[i][1:]   #The rest of the elements in each Column are data strings.
-            #print ("This is the title: ",cTitle)
-            #print (" This is the Data as strings: ",cData)
-            cData=list(map(int,cData))
-        #    print (" This is the Data as integers: ",cData)
-            #cData=list(map(int,cData[i]))   #Make the list of strings into integers.
-            cMean= mean(cData)
-            cStdev= stdev(cData)
-            cVar= variance(cData)
-            cMin=min(cData)
-            cMax=max(cData)
-        #    print (" This is the mean: ",cMean)
-        #    print (" This is the Standard Deviation: ",cStdev)
-        #    print (" This is the Variance: ",cVar)
-        #    print (" This is the min: ",cMin)
-        #    print (" This is the max: ",cMax,"\n")
+            if "Diff" in cTitle:
+                cData=niceData[i][1:]   #The rest of the elements in each Column are data strings.
+                cData=list(map(int,cData))
+                cMean= newMean(cData)   #we had to write a new mean function so that it did not include the zeros in the list.
+                cStdev= stdev(cData)
+                cVar= variance(cData)
+                cMin=min(cData)
+                cMax=max(cData)
+                print("Title: {}".format(cTitle), file=text_file)   #Print all statistics onto the Output.txt file
+                print("Data: {}".format(cData), file=text_file)
+                print("Mean: {}".format(cMean), file=text_file)
+                print("Standard Deviation: {}".format(cStdev), file=text_file)
+                print("Variance: {}".format(cVar), file=text_file)
+                print("Min: {}".format(cMin), file=text_file)
+                print("Max: {}".format(cMax), file=text_file)
+                print("\n", file=text_file)
             i+=1
-            print("Title: {}".format(cTitle), file=text_file)
-            print("Data: {}".format(cData), file=text_file)
-            print("Mean: {}".format(cMean), file=text_file)
-            print("Standard Deviation: {}".format(cStdev), file=text_file)
-            print("Variance: {}".format(cVar), file=text_file)
-            print("Min: {}".format(cMin), file=text_file)
-            print("Max: {}".format(cMax), file=text_file)
-            print("\n", file=text_file)
 
-def removeBlanks(bList): #bList is a list of lists with elements that may be empty strings, we want to remove these empty strings.
+def removeBlanks(bList): #bList is a 2d list with elements that may be empty strings, we want to remove these empty strings.
     wBlanks=bList
     i=0
     while(i<len(wBlanks)):
@@ -77,28 +78,23 @@ def removeBlanks(bList): #bList is a list of lists with elements that may be emp
             i+=1
     return wBlanks
 
+def getDelta(list1,list2,threshold): #
+    if not (len(list1)==len(list2)):
+        raise Exception('The two Lists should be the same dimensions')
+        return []
+
 root = Tk()
 root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("csv files","*.CSV"),("all files","*.*")))
 #print (root.filename)
 
 csvList=[]
 csvList=list(csv.reader(open(root.filename)))#This puts csv files into a 2d list
-print(csvList)
 
-print("Now without blanks:\n ")
-print(removeBlanks(csvList))
-
-print("Now without extra rows:\n ")
-print(truncateCsv(removeBlanks(csvList)))
-
-print("Now with a transpose:\n ")
-print(transposeCsv(truncateCsv(removeBlanks(csvList))))
-
-print("Now with data:\n ")
+#print(csvList)
+#print(removeBlanks(csvList))
+#print("Now without extra rows:\n ")
+#print(truncateCsv(removeBlanks(csvList)))
+#print("Now with a transpose:\n ")
+#print(transposeCsv(truncateCsv(removeBlanks(csvList))))
+#print("Now with data:\n ")
 print(printData(transposeCsv(truncateCsv(removeBlanks(csvList)))))
-
-#print("Rows: =",len(csvList),"\nColumns: ",len(csvList[0]))
-#print("last Row Size: ", len(csvList[len(csvList)-1]),"\n")
-
-#truncatedList=truncateCsv(csvList)
-#print (transposeCsv(truncatedList))
