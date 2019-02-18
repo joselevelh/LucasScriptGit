6f73c6b9e927a932c3360572f99f6777479683f7
joselevelh <43297291+joselevelh@users.noreply.github.com>
2019-02-18 11:07:27 -0500

    Titles and Data points as strings
    At this point we are able to isolate and print the titles and the data points as strings.

--- OpenCSV.py
+++ OpenCSV.py
@@ -4,18 +4,6 @@ import csv
 from statistics import mean
 
 
-root = Tk()
-root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("csv files","*.CSV"),("all files","*.*")))
-#print (root.filename)
-
-csvList=[]
-csvList=list(csv.reader(open(root.filename)))#This puts csv files into a 2d list
-#print(csvList)
-
-print("Rows: =",len(csvList),"\nColumns: ",len(csvList[0]))
-print("last Row Size: ", len(csvList[len(csvList)-1]),"\n")
-
-
 def truncateCsv(uList):   #Truncates the 2d array until all Columns have the same number of Rows
     tempList=uList
     while(len(tempList[0])!=len(tempList[len(tempList)-1])):
@@ -25,14 +13,13 @@ def truncateCsv(uList):   #Truncates the 2d array until all Columns have the sam
     print("last Row Size: ", len(tempList[len(tempList)-1]),"\n")
     return tempList
 
-
 def transposeCsv(OldList): #transpose the 2d list given
     newlist = []
     i = 0
-    while i < len(OldList[i]):
+    while i < len(OldList[0]):
         j = 0
         vec = []
-        while j < len(OldList)-1:
+        while j < len(OldList):
             vec.append(OldList[j][i])
             j = j + 1
         newlist.append(vec)
@@ -41,33 +28,61 @@ def transposeCsv(OldList): #transpose the 2d list given
 
 def printData(niceData):    # parse the truncated 2d list for relevant data and print it nicely.
     numCols=len(niceData)   #Amount of columns in niceData
-    #numRows=len(niceData[0])    #Amount of rows in the first Column Column
-    colCounter=1    #We want to skip the first Column because it is the time Column and therefore does not contain integer data.
-    while (colCounter<numCols):   #go through every Column.
-        ColumnTitle=niceData[colCounter][0]   #The first element of each Column is the title of that Column.
-        ColumnData=niceData[colCounter][1:]   #The rest of the elements in each Column are data strings.
-        n=0
-        while(n<len(ColumnData)-1):
-            ColumnData[n]=list(filter(None,ColumnData[n]))   #Filter the list for empty strings.
-            n+=1
-        ColumnData=list(map(int,ColumnData[0]))   #Make the list of strings into integers.
-        ColumnMean= mean(ColumnData[0])
+    i=1    #We want to skip the first Column because it is the time Column and therefore does not contain integer data.
+    while (i<numCols):   #go through every Column.
+        cTitle=niceData[i][0]   #The first element of each Column is the title of that Column.
+        cData=niceData[i][1:]   #The rest of the elements in each Column are data strings.
+        print ("This is the title: ",cTitle)
+        print (" This is the Data: ",cData,"\n")
+        #cData=list(map(int,cData[i]))   #Make the list of strings into integers.
+        #cMean= mean(cData[i])
         #ColumnStdev= statistics.stdev(ColumnData)
         #ColumnVar= statistics.variance(ColumnData)
         #ColumnMin=min(ColumnData)
         #ColumnMax=max(ColumnData)
-        colCounter+=1
-    with open("Output.txt", "w") as text_file:
-        print("Column: {}".format(ColumnData), file=text_file)
-        print("Mean: {}".format(ColumnMean), file=text_file)
+        i+=1
+#    with open("Output.txt", "w") as text_file:
+#        print("Column: {}".format(ColumnData), file=text_file)
+#        print("Mean: {}".format(ColumnMean), file=text_file)
 
-def printData2(niceData):
-    thisList=niceData
-    thisList=filter(None,thisList[5])
-    return thisList
+def removeBlanks(bList): #bList is a list of lists with elements that may be empty strings, we want to remove these empty strings.
+    wBlanks=bList
+    i=0
+    while(i<len(wBlanks)):
+        j=0
+        while(j<len(wBlanks[i])):
+            if not (wBlanks[i][j]):
+                del wBlanks[i][j]
+            else:   #We must check the same position again if we remove a blank because all the elements in the list have now shifted.
+                j+=1
+        if not (wBlanks[i]):#check if the list is empty.
+            del wBlanks[i]
+        else:
+            i+=1
+    return wBlanks
 
+root = Tk()
+root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("csv files","*.CSV"),("all files","*.*")))
+#print (root.filename)
 
-truncatedList=truncateCsv(csvList)
-print (transposeCsv(truncatedList))
-#printData(transposeCsv(truncatedList))
-print
+csvList=[]
+csvList=list(csv.reader(open(root.filename)))#This puts csv files into a 2d list
+print(csvList)
+
+print("Now without blanks:\n ")
+print(removeBlanks(csvList))
+
+print("Now without extra rows:\n ")
+print(truncateCsv(removeBlanks(csvList)))
+
+print("Now with a transpose:\n ")
+print(transposeCsv(truncateCsv(removeBlanks(csvList))))
+
+print("Now with data:\n ")
+print(printData(transposeCsv(truncateCsv(removeBlanks(csvList)))))
+
+#print("Rows: =",len(csvList),"\nColumns: ",len(csvList[0]))
+#print("last Row Size: ", len(csvList[len(csvList)-1]),"\n")
+
+#truncatedList=truncateCsv(csvList)
+#print (transposeCsv(truncatedList))
